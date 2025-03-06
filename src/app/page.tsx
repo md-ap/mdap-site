16440abc062ca5a34f14b01a2a6a759edca18932
animico <animico@gmail.com>
2025-03-06 04:24:55 -0800

    Demo Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <p>MDAP</p>
+    <div className="flex flex-col">
+      <p>
+        Mindful Data Audiovisual Project (MDAP) explores hybrid projects that combine experimental practices with biofeedback sensors, musical composition and audiovisual performance. He is a musician, composer and creative programmer, who combines technology and art to generate music using body movement, brain waves, heart rate and breathing.
+      </p>
+
+      <p>
+        Through various software algorithms and biofeedback sensors it uses the human body and its biological processes to create audiovisual pieces, inviting the user to explore themselves as a composer and creator, resulting in a wide range of effects on the participant, which include sound therapy, mindfulness and psychomotor exploration.
+      </p>
+
+      <p>
+        MDAP has toured Mexico, Germany, the Netherlands, Spain, the United States, one of its most recent presentations has been at the LA Design Festival, the Mutek Mexico festival and in Italy, Venice at the inauguration of avilion 0/4 "Beyond Code and Time" organized by Mediations Biennale and World Design Capital
+      </p>
+    </div>
   );
 }
---
+++ src/app/presentations/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+export default function Work() {
+  return (
+    <div className="w-full">      
+      <div className="grid gap-4 font-sans">
+        <ul className="space-y-1 md:space-y-1 space-y-3">
+          <li>World Design Policy Conference, San Diego, USA and Tijuana, Mexico</li>
+          <li>Symbiotic Borders for World Design Capital 2024, San Diego, USA and Tijuana, Mexico</li>
+          <li>Tijuana Design Week</li>
+          <li>Creative Mornings Tijuana</li>
+          <li>Festival Internacional de Arte Contemporaneo de León (FICA)</li>
+          <li>Mutek Festival & Neurofeedback Lab Masterclass, Mexico City, Mexico</li>
+          <li>Mediations Biennale, Beyond Code and Time Venice, Italy</li>
+          <li>Neurofeedback Lab, Avant Dev Mexico City, Mexico</li>
+          <li>Coaxial Arts Los Angeles, USA</li>
+          <li>Sydney Fringe Festival, Tu Casa Mi Casa Sydney, Australia</li>
+          <li>Art, Design and the Community Masterclass, California State University Los Angeles, USA</li>
+          <li>Galería Contrast Barcelona, Spain</li>
+          <li>Festival Flamenc-On Santa Coloma de Gramanet, Spain</li>
+          <li>Kunsthalle, California Dreams Hannover, Germany</li>
+          <li>8sixa Amsterdam, Netherlands</li>
+          <li>OT301 Amsterdam, Netherlands</li>
+          <li>World Design Capital 2024 Finalist Event, The Rady Shell at Jacobs Park San Diego, USA</li>
+          <li>Centro Cultural Tijuana, CECUT Tijuana, Mexico</li>
+          <li>Centro Centro Estatal de las Artes, CEART Tijuana, Mexico</li>
+          <li>Blackbox Tijuana, Mexico</li>
+        </ul>
+      </div>
+    </div>
+  );
+} 
